0c83f9a793596b12196edf088de285e16b358364
Cem <124288976+Cem9901@users.noreply.github.com>
2023-12-07 15:32:15 +0300

    Update x.py

--- x.py
+++ x.py
@@ -1,27 +1,17 @@
-# Özellik önemini alın
-feature_importance = xg_reg.feature_importances_
-
-# Özellik isimlerini ve önem sıralarını bir veri çerçevesine yerleştirin
-feature_importance_df = pd.DataFrame({'Feature': X_train.columns, 'Importance': feature_importance})
-
-# Özellikleri önem sırasına göre sıralayın
-feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
-
-# Özellik önemini görselleştirin
-plt.figure(figsize=(12, 8))
-sns.barplot(x='Importance', y='Feature', data=feature_importance_df, palette='viridis')
-plt.title('XGBoost - Özellik Önemi')
-plt.xlabel('Özellik Önemi')
-plt.ylabel('Özellik')
+# Hata dağılım grafiği
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test, errors, alpha=0.5)
+plt.title('Hata Dağılım Grafiği')
+plt.xlabel('Gerçek Değerler')
+plt.ylabel('Hatalar')
 plt.show()
 
-# Kümülatif önem sıralamasını görselleştirin
-cumulative_importance = feature_importance_df['Importance'].cumsum()
 
+
+# Gerçek değerler ile tahmin edilen değerlerin karşılaştırılması
 plt.figure(figsize=(10, 6))
-plt.plot(range(1, len(cumulative_importance) + 1), cumulative_importance, marker='o', linestyle='--', color='b')
-plt.title('XGBoost - Kümülatif Özellik Önemi')
-plt.xlabel('Toplam Özellik Sayısı')
-plt.ylabel('Kümülatif Özellik Önemi')
-plt.grid(True)
+plt.scatter(y_test, y_pred, alpha=0.5)
+plt.title('Gerçek Değerler ve Tahminlerin Karşılaştırılması')
+plt.xlabel('Gerçek Değerler')
+plt.ylabel('Tahmin Edilen Değerler')
 plt.show()
